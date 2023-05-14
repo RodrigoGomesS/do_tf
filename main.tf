bb61bb472d9f091d526a4708d92227a230a11c58
Rodrigo Gomes <cklrod@hotmail.com>
2023-05-14 14:01:26 -0300

    feat: Adicionar .gitignore e remover arquivos desnecessários
    Adicionar o arquivo .gitignore para ignorar o arquivo .terraform.lock.hcl e o arquivo main.tf, que não são necessários para o controle de versão.
    
    Isso ajudará a manter o histórico de commits limpo e evitará a adição acidental de arquivos desnecessários ao repositório.

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "digitalocean" {
+  
+}
+
+terraform{
+    required_providers{
+        digitalocean ={
+            source = "digitalocean/digitalocean"
+        }
+    }
+  }
+
+  resource "digitalocean_droplet" "web" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-1"
+  region = "nyc2"
+  size   = "s-1vcpu-1gb"
+}
